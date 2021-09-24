4cac14bf95dbddd74b2134cab34defba2b300500
Maria-Luengo <77382852+Maria-Luengo@users.noreply.github.com>
2021-09-24 08:27:47 +0200

    Create carateresespeciales.java

---
+++ caracteresEspeciales/src/carateresespeciales.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Maria333
+ */
+public class carateresespeciales {
+    public static void main(String[] args) {
+        var concat1="Buenos";
+        var concat2="días!";
+        
+        var nombre = "María";
+        System.out.println(concat1+ " " + concat2);
+        System.out.println("Caracteres especiales java");
+        System.out.println("Comillas simples \'"+nombre+"\'");
+        System.out.println("Comillas dobles \""+nombre+"\"");
+        System.out.println("Tabulador \t"+nombre);
+        System.out.println("Barra invertida \\"+nombre+"\\");
+        System.out.print("Salto de línea quitando el ln \n");
+        System.out.print("Retorno de línea :) \b");
+        System.out.println("Voy al principio de la línea \r");
+    }
+    
+}
